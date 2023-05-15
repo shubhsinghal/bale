3f863b9dc5f0148174217c6986e50bdf28fa9d8b
Shubhendra Pal Singhal <ssinghal74@login-phoenix-slurm-2.pace.gatech.edu>
2023-05-14 22:46:44 -0400

    k

--- src/bale_classic/convey/putport.c
+++ src/bale_classic/convey/putport.c
@@ -152,6 +152,8 @@ static buffer_t*
 putp_borrow(porter_t* self)
 {
   put_porter_t* putp = (put_porter_t*) self;
+  FILE *fp = fopen("print-shubh.txt", "a+");
+  fprintf(fp, "i_pending: %d, n_pending: %d\n", putp->i_pending, putp->n_pending);
   if (putp->i_pending == putp->n_pending)
     putp_scan_receipts(putp);
   int i = putp->i_pending;
