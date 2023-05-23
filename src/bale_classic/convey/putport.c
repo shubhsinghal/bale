c8ea8496dcaa165c99ffb0c3b9fd56f66b94693f
Shubhendra Pal Singhal <ssinghal74@atl1-1-02-004-6-2.pace.gatech.edu>
2023-05-23 17:53:46 -0400

    k

--- src/bale_classic/convey/putport.c
+++ src/bale_classic/convey/putport.c
@@ -519,6 +519,8 @@ porter_new(int n, int32_t relative[n], int my_rank,
     return NULL;
   const bool dynamic = options & convey_opt_DYNAMIC;
   const bool steady = options & convey_opt_PROGRESS;
+  FILE *fp = fopen("print-shubh.txt", "a+");
+  fprintf(fp, "steady:%d", steady);
   const bool local = options & porter_opt_LOCAL;
   const bool compress = options & convey_opt_COMPRESS;
   const bool blocking = options & convey_opt_BLOCKING;
