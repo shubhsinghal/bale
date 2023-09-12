68ccdf659c7f26a96cd4ac04a19f8141d7dcbe7e
Shubhendra Pal Singhal <ssinghal74@login-phoenix-slurm-1.pace.gatech.edu>
2023-09-12 06:08:14 -0400

    k

--- src/bale_classic/convey/putport.c
+++ src/bale_classic/convey/putport.c
@@ -342,9 +342,11 @@ local_send(porter_t* self, int dest, uint64_t level, size_t n_bytes,
     self->byte_count += n_bytes;
   }
 
+  int pe = putp->friends[dest];
+  shmem_put64((uint64_t*) &putp->received[rank], &signal, 1, pe);
   // Need local address of remote 'received' array
-  atomic_uint64_t* notify = nbrhood->signal_ptrs[dest] + rank;
-  *notify = signal;  // atomic_store
+  // atomic_uint64_t* notify = nbrhood->signal_ptrs[dest] + rank;
+  // *notify = signal;  // atomic_store
   return true;
 }
 
